e9a9e70142dad6ceae985a2209e601258e91b57b
IstinNew <tubaf.65178@gmail.com>
2025-01-27 13:19:27 +0100

    Revised Structure for Working Location for PerformanceAppraisal2025

--- apppa25.py
+++ apppa25.py
@@ -29,6 +29,7 @@ if sections == "Persönliche Informationen":
     st.write(f"<p style='color: {arcadis_colors['text']}; font-size: 18px;'> Name: Majumder, Shayak</p>", unsafe_allow_html=True)
     st.write(f"<p style='color: {arcadis_colors['text']}; font-size: 18px;'> Eintrittsdatum: 01.01.2025</p>", unsafe_allow_html=True)
     st.write(f"<p style='color: {arcadis_colors['text']}; font-size: 18px;'> Name der Führungskraft: Soldner, Dennis</p>", unsafe_allow_html=True)
+    st.write(f"<p style='color: {arcadis_colors['text']}; font-size: 18px;'> Arbeitsort: Freiberg (Sachs)</p>", unsafe_allow_html=True)
     
     # Additional Personal Information
     st.markdown("""
